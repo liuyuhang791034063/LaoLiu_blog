9292fe2b6c6f02108897a7372dbe9458a800b889
LaoLiu <liuyuhang791034063@qq.com>
2018-05-01 22:13:27 +0800

    修改设置

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,4 +19,5 @@ from django.conf.urls import url,include
 urlpatterns = [
     url(r'^admin/',admin.site.urls),
     url(r'^blog/',include(('blog.urls','blog') , namespace='blog')),
+    url(r'^account/', include(('account.urls','account'), namespace = 'account')),
 ]

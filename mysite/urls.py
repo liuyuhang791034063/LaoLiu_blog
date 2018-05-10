36f943e74f5989a78c162963b9201a1bd52a3f26
LaoLiu <liuyuhang791034063@qq.com>
2018-05-10 21:40:52 +0800

    添加模板标签

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     #url(r'^pwd_reset/', include(('password_reset.urls','pwd_reset') ,namespace='pwd_reset')),
     url(r'^article/', include(('article.urls','article'),namespace= 'article')),
     url(r'^home/', TemplateView.as_view(template_name="home.html"),name="home"),
+    url(r'', TemplateView.as_view(template_name="home.html"), name="null"),
+
 ]

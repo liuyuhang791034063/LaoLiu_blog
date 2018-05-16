a07a2a39c82f497597016524a71f127df982f63c
liuyuhang791034063 <liuyuhang791034063@qq.com>
2018-05-16 22:05:23 +0800

    新增自定义字段

---
+++ course/fields.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 18-5-16 下午9:46
+# @Author  : GodFather
+# @Email   : liuyuhang791034063@qq.com
+# @File    : fields.py
+# @Software: PyCharm
+
+from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
+
+class OrderField(models.PositiveIntegerField):
+    def __init__(self, for_fields = None, *args, **kwargs):
+        self.for_fields = for_fields
+        super(OrderField,self).__init__(*args, **kwargs)
+
+    def pre_save(self, model_instance, add):
+        if getattr(model_instance,self.attname) is None:
+            try:
+                qs = self.model.objects.all()
+                if self.for_fields:
+                    query = {field: getattr(model_instance, field) for field in self.for_fields}
+                    qs = qs.filter(**query)
+                last_item = qs.latest(self.attname)
+                value = last_item.order + 1
+            except ObjectDoesNotExist:
+                value = 0
+            setattr(model_instance, self.attname, value)
+            return value
+        else:
+            return super().pre_save(model_instance,add)

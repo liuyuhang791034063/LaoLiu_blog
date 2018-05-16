aa636a8cb784204628fc4314c24ab8d2d1e3974d
liuyuhang791034063 <liuyuhang791034063@qq.com>
2018-05-16 14:46:24 +0800

    忽略虚拟环境上传

--- course/views.py
+++ course/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
+
+from .models import Course
 
 class AboutView(TemplateView):
     template_name = 'course/about.html'
+
+class CourseListView(ListView):
+    model = Course # 可获取相应模型的数据库表
+    context_object_name = "courses" # 模板变量名称
+    template_name = 'course/course_list.hmtl' 
